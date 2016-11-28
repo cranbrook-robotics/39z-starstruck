3946c5c55ed4bf299ed5c90012129b013a400f07
th3jmast3r <th3jmast3r@gmail.com>
2016-11-28 14:39:25 -0500

    new accDebug file

---
+++ accDebug.c
@@ -0,0 +1,94 @@
+#pragma config(UserModel, "39zconfig.c")
+#pragma platform(VEX)
+
+
+int initX, initY, initH; //Initial X, Y, Heading
+
+
+float curXVel = 0, curYVel = 0, curXPos, curYPos, curHeading; //Current X Velociy, Y Velocity, X Position, Y Position, Heading
+float interval = 0.075; //Interval is in SECONDS
+
+
+typedef enum AccAxis { XAxis, YAxis }; //Axes Accelerometer Uses
+tSensors accPorts[] = {acX, acY}; //Ports Accelerometer Uses
+float accBias[] = {0, 0};
+
+//Calculate Value the Accelerometer Returns
+float getAcc(AccAxis axis)
+{
+	return SensorValue(accPorts[axis] - accBias[axis])*0.5234 - 1071.7; //Conversion from Accelerometer Returns to in/s^2
+}
+
+//Initializes the Accelerometer by Calculating Bias
+void initAcc(){
+	curXVel = 0;
+	curYVel = 0;
+	float xC = 0;
+	float yC = 0;
+	for (int i = 0; i < 50; i++)
+	{
+		xC += (accPorts[YAxis] - 2047.5);
+		yC += (accPorts[XAxis] - 2047.5);
+		delay(100);
+	}
+	accBias[XAxis] = xC/50;
+	accBias[YAxis] = yC/50;
+}
+
+//Calculates Initial Position and Heading based on current side and team
+void initPos()
+{
+	initX = 0;
+	initY = 0;
+	initH = 0;
+	curHeading = initH;
+	SensorValue(gyro) = initH;
+	curXPos = initX;
+	curYPos = initY;
+}
+
+void initGyro(){
+	SensorType[gyro] = sensorNone;
+	wait1Msec(1000);
+	SensorType[gyro] = sensorGyro;
+	wait1Msec(2000);
+	SensorValue(gyro) = 0;
+}
+
+//Tracks Current Location, Velocity, and Heading while the track task is being run
+task track()
+{
+	while (true)
+	{
+		curXVel += getAcc(XAxis)*interval; // m/s^2 * s/1000 * unit conversion
+		curYVel += getAcc(YAxis)*interval;
+		curXPos += curXVel*interval;
+		curYPos += curYVel*interval;
+		curHeading = SensorValue(gyro)/10.;
+		delay (interval*1000);
+	}
+}
+
+
+void pre_auton()
+{
+	SensorValue(initIndicator) = 1;
+	initAcc();
+	initGyro();
+	SensorValue(initIndicator) = 0;
+	clearDebugStream();
+}
+
+task main()
+{
+	pre_auton();
+	initPos();
+	startTask(track);
+	while(true)
+	{
+		writeDebugStreamLine("CurXAcc: %f", getAcc(XAxis));
+		writeDebugStreamLine("CurYAcc: %f", getAcc(YAxis));
+		delay(500);
+
+	}
+}
